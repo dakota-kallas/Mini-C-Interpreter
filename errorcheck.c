40886fe9dee5608b17772bfe596b1e57be643eb4
Dakota Kallas <79180508+dakota-kallas@users.noreply.github.com>
2022-04-05 13:58:46 -0500

    Added error checking file.
    Added a test file that will demonstrate error handling by the TypeChecker and Interpreter.

---
+++ errorcheck.c
@@ -0,0 +1,26 @@
+int main ( ) {
+  int n;
+  int i;
+  int factorial;
+  bool b;
+  {
+    n = 3;
+    i = -1;
+    factorial = 2;
+    b = false || false;
+    if(factorial > n) {
+      int j;
+      bool j;
+      x = 10;
+      i = 2;
+      i = x;
+      i = 2 < true;
+      if(10 + 1) {
+        n = false;
+        while(10/2) {
+          x = 7;
+        }
+      }
+    }
+  }
+}
